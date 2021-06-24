e444ffb94b5bf6ce050e9f25cabd2f2de583eb87
Axel Hernández <axeljaredhernandezservellon@gmail.com>
2021-06-24 16:28:59 -0600

    ADD:appointment1

---
+++ Vaccination/Vaccination/View/Frmappointment.Designer.cs
@@ -0,0 +1,217 @@
+﻿using System.ComponentModel;
+
+namespace Vaccination
+{
+    partial class Frmappointment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frmappointment));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cbxCabin = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDuiR = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.cbxCabin);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtDuiR);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.dateTimePicker3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(441, 218);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cita";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(387, 168);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(28, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cbxCabin
+            // 
+            this.cbxCabin.FormattingEnabled = true;
+            this.cbxCabin.Location = new System.Drawing.Point(158, 108);
+            this.cbxCabin.Name = "cbxCabin";
+            this.cbxCabin.Size = new System.Drawing.Size(204, 23);
+            this.cbxCabin.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Location = new System.Drawing.Point(0, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 23);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Cabina:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtDuiR
+            // 
+            this.txtDuiR.Enabled = false;
+            this.txtDuiR.Location = new System.Drawing.Point(158, 82);
+            this.txtDuiR.Name = "txtDuiR";
+            this.txtDuiR.Size = new System.Drawing.Size(202, 21);
+            this.txtDuiR.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label4.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label4.Location = new System.Drawing.Point(0, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Dui del ciudadano:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.button1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.button1.Location = new System.Drawing.Point(235, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 35);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Crear cita";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker3.Location = new System.Drawing.Point(158, 55);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.ShowUpDown = true;
+            this.dateTimePicker3.Size = new System.Drawing.Size(202, 21);
+            this.dateTimePicker3.TabIndex = 5;
+            this.dateTimePicker3.Value = new System.DateTime(2021, 6, 22, 21, 54, 0, 0);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label3.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label3.Location = new System.Drawing.Point(6, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Hora de primera cita:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(158, 29);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(202, 21);
+            this.dateTimePicker2.TabIndex = 3;
+            this.dateTimePicker2.Value = new System.DateTime(2021, 6, 22, 0, 0, 0, 0);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label2.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label2.Location = new System.Drawing.Point(6, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Fecha de primera cita:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Frmappointment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MintCream;
+            this.ClientSize = new System.Drawing.Size(465, 242);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Frmappointment";
+            this.Text = "Registrando Cita";
+            this.Load += new System.EventHandler(this.Frmappointment_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxCabin;
+
+        private System.Windows.Forms.TextBox txtDuiR;
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.GroupBox groupBox1;
+
+        #endregion
+    }
+}
---
+++ Vaccination/Vaccination/View/Frmappointment.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
+using Vaccination.SqlServerContext;
+
+namespace Vaccination
+{
+    public partial class Frmappointment : Form
+    {
+        
+        public Frmappointment(string Dui)
+        {
+            InitializeComponent();
+           
+            txtDuiR.Text = Dui;
+
+        }
+        
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            var db = new Proyecto_DB_POOContext();
+
+            Appointment apo = new Appointment()
+            {
+                Dui = txtDuiR.Text,
+                FirstDate = dateTimePicker2.Value,
+                FirstHour = dateTimePicker3.Value.TimeOfDay,
+                IdCabin = ((Cabin) cbxCabin.SelectedItem).Id,
+                VaccinationPlace = ((Cabin) cbxCabin.SelectedItem).AddressCabin
+            };
+            
+                db.Add(apo);
+                db.SaveChanges();
+                MessageBox.Show("Cita Registrada!!!", "Sistema de registro - Citas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+                MessageBox.Show("vacundo:) ","Proceso de Vacunacion - Covid-19", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Hide();
+                this.Close();
+                
+                Frmappointment2 form = new Frmappointment2(txtDuiR.Text,dateTimePicker2.Value.AddDays(49));
+                form.ShowDialog();
+            }
+
+
+        private void Frmappointment_Load(object sender, EventArgs e)
+        {
+            var db = new Proyecto_DB_POOContext();
+            List<Cabin> cabin = db.Cabins
+                .ToList();
+            cbxCabin.DataSource = cabin;
+            cbxCabin.DisplayMember = "AddressCabin";
+            cbxCabin.ValueMember = "Id";
+        }
+    }
+    }
